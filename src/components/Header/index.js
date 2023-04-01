7894d3f54a7f513025296b62c1a5931d29baba9a
ShivamGithub120999 <sainishivam877@gmail.com>
2023-04-01 14:30:40 +0530

    Sample Commit Reverted

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         {/* <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand> */}
-        <Link to="/" className="navbar-brand">Admin Dashboardd</Link>
+        <Link to="/" className="navbar-brand">Admin Dashboard</Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
